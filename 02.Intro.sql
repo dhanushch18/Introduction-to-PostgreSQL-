a2d2cd6855f267f7d6646a9bc31c0aa6d0581fbf
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-05-26 20:52:18 +0530

    Create 02.Intro.sql

---
+++ 02.Intro.sql
@@ -0,0 +1,10 @@
+SELECT * FROM cars
+ORDER BY year;  
+
+Result:
+ brand  | model   | year
+--------+---------+------
+ Ford   | Mustang | 1964
+ Volvo  | p1800   | 1968
+ Toyota | Celica  | 1975
+ BMW    | M1      | 1978
