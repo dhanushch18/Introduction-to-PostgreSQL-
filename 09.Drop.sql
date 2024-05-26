534971465df8d036a64921fabbe8892cdadec67a
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-05-26 23:31:57 +0530

    Create 09.Drop.sql

---
+++ 09.Drop.sql
@@ -0,0 +1,11 @@
+To remove a column from a table, we have to use the ALTER TABLE statement.
+
+The ALTER TABLE statement is used to add, delete, or modify columns in an existing table.
+
+The ALTER TABLE statement is also used to add and drop various constraints on an existing table.
+
+DROP COLUMN
+We want to remove the column named color from the cars table.
+
+ALTER TABLE cars
+DROP COLUMN color;
