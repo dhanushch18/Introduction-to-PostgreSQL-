6be10aa197789147a330f2a41c6c9093654980cc
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-05-29 22:16:40 +0530

    Create 14.SelectData.sql

---
+++ 14.SelectData.sql
@@ -0,0 +1,9 @@
+SELECT customer_name, country FROM customers;
+
+customer_name             |   country
+--------------------------------------+-------------
+ Alfreds Futterkiste                  | Germany
+ Ana Trujillo Emparedados y helados   | Mexico
+ Antonio Moreno Taquera               | Mexico
+ Around the Horn                      | UK
+ Berglunds snabbkoep                 
