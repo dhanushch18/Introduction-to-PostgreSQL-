60b297a3d86fba483977b864d85e0c330e721412
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-05-30 22:16:52 +0530

    Create 15.Where.sql

---
+++ 15.Where.sql
@@ -0,0 +1,9 @@
+SELECT * FROM customers
+WHERE city = 'London';
+
+
+customer_id |     customer_name     |   contact_name    |           address            |  city  | postal_code | country
+-------------+-----------------------+-------------------+------------------------------+--------+-------------+---------
+           4 | Around the Horn       | Thomas Hardy      | 120 Hanover Sq.              | London | WA1 1DP     | UK
+          11 | Bs Beverages          | Victoria Ashworth | Fauntleroy Circus            | London | EC2 5NT     | UK
+          16 | Consolidated Holdings Inc attention 
