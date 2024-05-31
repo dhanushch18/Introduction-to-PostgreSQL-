9dd6cb266023a0133c9f68d7d5f8fb78f2d1061d
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-05-31 07:39:45 +0530

    Create 16.Order.sql

---
+++ 16.Order.sql
@@ -0,0 +1,8 @@
+SELECT * FROM products
+ORDER BY price;
+
+product_id |           product_name           | category_id |         unit         | price
+------------+----------------------------------+-------------+----------------------+--------
+         33 | Geitost                          |           4 | 500 g                |   2.50
+         24 | Guarani Fantastica               |           1 | 12 - 355 ml cans     |   4.50
+         13 | Konbu
